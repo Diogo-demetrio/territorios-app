ff88ea8548a8b92589fd27db6d2a4d742fbb3911
Diogo Demetrio <funcionow.connect@gmail.com>
2026-07-22 23:50:18 -0300

    corrige enderecos inativos e atualiza totais

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = "Territórios";
-export const APP_VERSION = "v1.0.1";
-export const BUILD_DATE = "21/07/2026";
+export const APP_VERSION = "v1.0.2";
+export const BUILD_DATE = "22/07/2026";
